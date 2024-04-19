70a68e6d82f90f5c62782551217f96d39d20b00e
Abdiel-88 <abdiel.f@outlook.com>
2024-04-19 00:57:23 -0400

    more import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+""" Creates the app_views template"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+# Creates a Blueprint which the rest of our pages will run through
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
+# Must import individual files below so as not to circular import
+
+from api.v1.views.index import *
 from api.v1.views.states import *
-# Include other imports similarly if there are more view files
